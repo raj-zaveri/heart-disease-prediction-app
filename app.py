93e7f51cceef76c337d526a3dbd0e7ffdd683827
raj-zaveri <167971902+raj-zaveri@users.noreply.github.com>
2025-04-29 13:24:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -63,19 +63,19 @@ if page == "Heart Disease Prediction":
     col1, col2, col3 = st.columns(3)
 
     with col1:
-        age = st.number_input('Age', 20, 100, step=1)
-        trestbps = st.number_input('Resting Blood Pressure (mm Hg)', 80, 200, step=1)
-        chol = st.number_input('Serum Cholestoral (mg/dl)', 100, 600, step=1)
-        fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl', [0, 1])
+        age = st.slider('Age', 20, 100, 50)
+        trestbps = st.slider('Resting Blood Pressure (mm Hg)', 80, 200, 120)
+        chol = st.slider('Serum Cholestoral (mg/dl)', 100, 600, 200)
+        fbs = st.selectbox('Fasting Blood Sugar > 120 mg/dl (1 = True; 0 = False)', [0, 1])
 
     with col2:
-        sex = st.selectbox('Sex', [0, 1])
+        sex = st.selectbox('Sex (0 : Female; 1 : Male )', [0, 1])
         cp = st.selectbox('Chest Pain Type (0-3)', [0, 1, 2, 3])
         restecg = st.selectbox('Resting ECG (0-2)', [0, 1, 2])
-        thalach = st.number_input('Max Heart Rate Achieved', 70, 210, step=1)
+        thalach = st.slider('Max Heart Rate Achieved', 60, 250, 150)
 
     with col3:
-        exang = st.selectbox('Exercise Induced Angina', [0, 1])
+        exang = st.selectbox('Exercise Induced Angina (1 = yes; 0 = no)', [0, 1])
         oldpeak = st.number_input('Oldpeak (ST depression)', 0.0, 6.0, step=0.1)
         slope = st.selectbox('Slope of Peak Exercise ST Segment (0-2)', [0, 1, 2])
         ca = st.selectbox('Number of Major Vessels Colored (0-3)', [0, 1, 2, 3])
@@ -106,7 +106,7 @@ if page == "Heart Disease Prediction":
 
         st.markdown("---")
         st.subheader("🔎 Risk Probability")
-        st.write(f"**Probability of Heart Disease:** {prediction_proba:.2f}")
+        st.write(f"**Probability of Heart Disease:** {prediction_proba * 100:.0f}%")
 
 
         # 🎯 Visualization
